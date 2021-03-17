1684eb3988e73e434549e95675dbb472bea19937
xeeny <xeeny@wp.pl>
2021-03-17 20:58:49 +0100

    impruve cp

--- cp.c
+++ cp.c
@@ -6,14 +6,32 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <getopt.h>
 
-static bool flag_n = false;
+static const char* progname = NULL;
 static bool flag_f = false;
+static bool flag_i = false;
+static bool flag_r = false;
+static bool flag_v = false;
+static struct option opts[] = {
+   {"help",         no_argument, 0, 'h' },
+   {"recursive",    no_argument, 0, 'r' },
+   {"verbose",      no_argument, 0, 'v' },
+   {"interactive",  no_argument, 0, 'i' },
+   {"force",        no_argument, 0, 'f' },
+   {0,              0,           0, 0   }
+};
+
+static void using(void) {
+    fprintf(stderr, "using:\n\t%s [-hifrRv] <file...>\n", progname);
+    exit(1);
+}
 
 static bool file_exists(const char* path) {
     int file = open(path, O_RDONLY);
-    if (file < 0)
+    if (file < 0) {
         return false;
+    }
     close(file);
     return true;
 }
@@ -27,7 +45,16 @@ static bool is_directory(const char* path) {
     return st.st_mode & S_IFDIR;
 }
 
+//#define MMAP_WORK
+#ifdef MMAP_WORK
+
 static void copy_file(const char* path_src, const char* path_dst) {
+    if ((flag_i && !flag_f) && file_exists(path_dst)) {
+        printf("do you want remove '%s': ", path_dst);
+        if (getchar() == 'y')
+            return;
+    }
+
     int input = open(path_src, O_RDONLY);
     if (input < 0) {
         perror("open");
@@ -45,26 +72,98 @@ static void copy_file(const char* path_src, const char* path_dst) {
         perror("mmap");
         exit(1);
     }
-    if ((!flag_n && flag_f) && file_exists(path_dst)) {
-        // TODO ask to confurm
-    }
+
     int output = open(path_dst, O_WRONLY | O_CREAT | O_TRUNC, st.st_mode);
     if (output < 0) {
+        perror("open");
+        exit(1);
+    }
+
+    write(output, buf, st.st_size);
+    munmap(buf, st.st_size);
+    close(input);
+    close(output);
+
+    if (flag_v)
+        printf("copy %s to %s\n", path_src, path_dst);
+}
+
+#else
+
+static void copy_file(const char* path_src, const char* path_dst) {
+    if ((flag_i && !flag_f) && file_exists(path_dst)) {
+        printf("do you want remove '%s': ", path_dst);
+        if (getchar() == 'y')
+            return;
+    }
+
+    FILE *file = fopen(path_src, "r");
+    if (file == NULL) {
         perror("fopen");
         exit(1);
     }
-    write(output, buf, st.st_size);
-    close(input);
-    close(output);
+
+    fseek(file, 0, SEEK_END);
+    size_t file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    
+    void* buffer = malloc(file_size);
+    if (buffer == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+
+    FILE *file_save = fopen(path_dst, "w");
+    if (file_save == NULL) {
+        perror("fopen");
+        exit(1);
+    }
+
+    fread(buffer, 1, file_size, file);
+    fwrite(buffer, 1, file_size, file_save);
+    free(buffer);
+    fclose(file);
+    fclose(file_save);
+
+    if (flag_v)
+        printf("copy %s to %s\n", path_src, path_dst);
 }
 
+#endif
+
+//static void recurvece_copy(const char* path_src, const char* path_dst) {
+//    // TODO copy folders.
+//}
+
 int main(int argc, char* argv[]) {
-    if (argc == 3) {
-        copy_file(argv[1], argv[2]);
+    progname = argv[0];
+    char ch;
+    while ((ch = getopt_long(argc, argv, "hifrRv", opts, NULL)) != -1) {
+        switch(ch) {
+        case 'i':
+            flag_i = true;
+            break;
+        case 'f':
+            flag_f = true;
+            break;
+        case 'r':
+            flag_r = true;
+            break;
+        case 'v':
+            flag_v = true;
+            break;
+        default:
+            using();
+        }
     }
-    if (argc > 3) {
+    argc -= optind;
+    argv += optind;
+
+    if (argc == 2) {
+        copy_file(argv[0], argv[1]);
+    } else if (argc > 2) {
         if (is_directory(argv[argc - 1])) {
-            for (int i = 1; i < argc - 1; i++) {
+            for (int i = 0; i < argc - 1; i++) {
                 char buf[0x4000] = {0}; 
                 // TODO fuster 
                 strcat(buf, argv[argc - 1]);
@@ -76,6 +175,8 @@ int main(int argc, char* argv[]) {
         } else {
             fprintf(stderr, "Destnation must be a directory.\n");
         }
+    } else {
+        using();
     }
     return 0;
 }
